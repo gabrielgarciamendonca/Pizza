93512dfe46c34d5409ec8fdcc12390fc31533aaa
Gabriel Garcia <gpm@lionx.com.br>
2021-09-21 17:29:04 -0300

    [INCLUDING][Typescript template into react-native project]

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
