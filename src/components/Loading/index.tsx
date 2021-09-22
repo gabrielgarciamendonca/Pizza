79f807072098cc4a68a59b8ee838df3179f3c77a
Gabriel Garcia <gpm@lionx.com.br>
2021-09-22 11:01:27 -0300

    [CREATING][Loading screen into project]

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import plateImg from '../../assets/plate.png';
+import { Container, Title } from './style';
+
+export function Loading() {
+  return (
+    <Container>
+      <Title>CARREGANDO</Title>
+    </Container>
+  );
+}
