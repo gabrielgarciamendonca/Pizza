afe4b3e5536990fa54b9764a16ee0cb384911bd2
Gabriel Garcia <gpm@lionx.com.br>
2021-09-22 10:58:47 -0300

    [APLYING][Fonts into project]

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,28 @@
+import { configureFonts } from 'react-native-paper';
+import { Font } from 'react-native-paper/lib/typescript/types';
+
+const regular: Font = { fontFamily: 'Roboto-Regular', fontWeight: 'normal' };
+const medium: Font = { fontFamily: 'Roboto-Medium', fontWeight: '500' };
+const light: Font = { fontFamily: 'Roboto-Light', fontWeight: '400' };
+const thin: Font = { fontFamily: 'Roboto-Thin', fontWeight: '300' };
+
+const fontConfig = {
+  native: {
+    regular: { ...regular },
+    medium: { ...medium },
+    light: { ...light },
+    thin: { ...thin },
+  },
+};
+
 export const theme = {
-    colors: {
-      orange: '#BF612A',
-      white: '#f7f7f7',
-      brown: '#591C27',
-      gray: '#afaca3',
-      red: '#A61B34',
-    },
-  
-    fonts: {
-      primary400: 'Ubuntu_400Regular',
-      primary500: 'Ubuntu_500Medium',
-      primary700: 'Ubuntu_700Bold',
-    }
+  colors: {
+    orange: '#BF612A',
+    white: '#f7f7f7',
+    brown: '#591C27',
+    gray: '#afaca3',
+    red: '#A61B34',
+  },
+
+  fonts: configureFonts(fontConfig),
 };
